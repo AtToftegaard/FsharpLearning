3712c0d5992fe4942ff521b1c7445c156ddcdd95
Andreas Toftegaard <ATF@kmd.dk>
2020-03-10 09:35:28 +0100

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Util
+
+[<EntryPoint>]
+let main argv =
+    
+    let rec BubbleSort (arr: int[]) = 
+        for i = 0 to arr.Length-2 do
+            if arr.[i] > arr.[i+1] then
+                Swap arr i (i+1)
+                BubbleSort arr
+    
+    let array = [| 1 .. 10 |]
+                |> Shuffle
+                |> PrintArray "UNSORTED:"
+    
+    BubbleSort array
+
+    PrintArray "SORTED:" array  |> ignore
+    
+    0 // return an integer exit code
+
+  //  let rec sort (a: int []) =
+  //  let mutable fin = true
+  //  for i in 0..a.Length-2 do
+  //  if a.[i] > a.[i+1] then
+  //    let t = a.[i]
+  //    a.[i] <- a.[i+1]
+  //    a.[i+1] <- t
+  //    fin <- false
+  //if not fin then sort a
+
---
+++ Util.fs
@@ -0,0 +1,20 @@
+﻿module Util
+
+open System
+
+let Swap (arr: int[]) x y  =
+    let temp = arr.[x]
+    arr.[x] <- arr.[y]
+    arr.[y] <- temp
+
+let PrintArray title (arr: int[]) = 
+    printf "\n %s \n %s \n" title (String.replicate 10 "-") 
+    for i = 0 to arr.Length-1 do
+        Console.WriteLine arr.[i]
+    arr
+
+let rand = new System.Random()    
+let Shuffle (a: int[]) =
+    [0..(a.Length-2)] 
+    |> Seq.iter(fun i -> Swap a i (rand.Next(0, Array.length a)))
+    a
